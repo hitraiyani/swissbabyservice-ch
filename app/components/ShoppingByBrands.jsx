2fa62474c1f91a6939948b847b3bfaad34227463
Ravipatelbg <114903445+Ravipatelbg@users.noreply.github.com>
2023-07-10 16:45:08 +0530

    Push the code for Homepage

--- app/components/ShoppingByBrands.jsx
+++ app/components/ShoppingByBrands.jsx
@@ -40,7 +40,7 @@ export function ShoppingByBrands({className, locale}) {
     >
       <div className="container">
         <div className="title-wrap">
-          <h2 className="text-[20px] font-bold text-left text-[#2380B1] flex items-center gap-[20px] mb-[40px]">
+          <h2 className="text-[20px] font-medium text-left text-[#2380B1] flex items-center gap-[20px] mb-[40px] font-['Open_Sans']">
             {translate('shop_by_brand', locale)}
             <span className="flex-1 border-b-[1px] border-[#3890bf] relative before:bg-no-repeat before:content-[''] before:inline-block before:w-5 before:h-5 before:bg-[url('https://cdn.shopify.com/s/files/1/0787/1352/0419/files/heart.png?v=1688561823')] before:absolute before:z-[2] before:-mt-1.5 before:right-[5px] md:before:right-[15px] before:top-full"></span>
           </h2>
@@ -62,14 +62,20 @@ export function ShoppingByBrands({className, locale}) {
               //   delay: 5000,
               //   disableOnInteraction: false,
               // }}
-              // breakpoints={{
-              //   0: {
-              //     autoHeight:true,
-              //   },
-              //   992: {
-              //     autoHeight:false,
-              //   },
-              // }}
+              breakpoints={{
+                0: {
+                  slidesPerView: 3,
+                  spaceBetween: 10,
+                },
+                768: {
+                  slidesPerView: 4,
+                  spaceBetween: 20,
+                },
+                992: {
+                  slidesPerView: 5,
+                  spaceBetween: 20,
+                },
+              }}
               className="shopping-by-brands-slider"
             >
               {brandData?.map((item, index) => {
