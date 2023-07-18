b2d4cdddb0f4b8a90f13fe41c86ce5a8ea8916b3
Ravipatelbg <114903445+Ravipatelbg@users.noreply.github.com>
2023-07-18 11:16:51 +0530

    Push the ocde For Homepage slider

--- app/components/NewInTheShop.jsx
+++ app/components/NewInTheShop.jsx
@@ -1,12 +1,5 @@
 import clsx from 'clsx';
-import {
-  Autoplay,
-  Navigation,
-  Pagination,
-  Scrollbar,
-  A11y,
-} from 'swiper';
-import {Swiper, SwiperSlide} from 'swiper/react';
+import Slider from 'react-slick';
 import {
   IconChevronRight,
   IconStar,
@@ -15,11 +8,84 @@ import {
   Link,
 } from '~/components';
 import {flattenConnection, Image, Money, useMoney} from '@shopify/hydrogen';
-import {discountedPer, isDiscounted, productTranslate, translate} from '~/lib/utils';
+import {
+  discountedPer,
+  isDiscounted,
+  productTranslate,
+  translate,
+} from '~/lib/utils';
 import {CompareAtPrice} from './CompareAtPrice';
-import { DiscountPercentage } from './DiscountPercentage';
+import {DiscountPercentage} from './DiscountPercentage';
+
+function SampleNextArrow(props) {
+  const {className, style, onClick} = props;
+  return (
+    <div
+      onClick={onClick}
+      id="swiper-button-next-new-in-Shop"
+      className="absolute right-0 md:right-[-20px] top-1/2 -translate-y-1/2 w-[40px] h-[45px] z-[1] transition-all duration-500 flex items-center justify-center cursor-pointer"
+    >
+      <img
+        className="w-full h-full object-contain"
+        src="https://cdn.shopify.com/s/files/1/0787/1352/0419/files/right-a.png?v=1688967073"
+        alt=""
+      />
+    </div>
+  );
+}
+
+function SamplePrevArrow(props) {
+  const {className, style, onClick} = props;
+  return (
+    <div
+      onClick={onClick}
+      id="swiper-button-prev-new-in-Shop"
+      className="absolute left-0 md:left-[-20px] top-1/2 -translate-y-1/2 w-[40px] h-[45px] z-[1] transition-all duration-500 flex items-center justify-center cursor-pointer"
+    >
+      <img
+        className="w-full h-full object-contain"
+        src="https://cdn.shopify.com/s/files/1/0787/1352/0419/files/left-a.png?v=1688967073"
+        alt=""
+      />
+    </div>
+  );
+}
 
 export function NewInTheShop({products, title, locale}) {
+  var settings = {
+    dots: false,
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
+    responsive: [
+      {
+        breakpoint: 1280,
+        settings: {
+          slidesToShow: 4,
+        },
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  };
   return (
     <section
       className={`new-in-Shop-section py-[20px] md:py-[30px] xl:py-[40px] 2xl:py-[50px]`}
@@ -32,47 +98,7 @@ export function NewInTheShop({products, title, locale}) {
           </h2>
         </div>
         <div className="relative new-in-Shop-slider">
-          <Swiper
-            modules={[Autoplay, Navigation, Pagination, Scrollbar, A11y]}
-            spaceBetween={28}
-            slidesPerView={4}
-            navigation={{
-              prevEl: '#swiper-button-prev-new-in-Shop',
-              nextEl: '#swiper-button-next-new-in-Shop',
-            }}
-            // pagination={{
-            //   clickable: true,
-            // }}
-            loop={true}
-           
-            // autoplay={{
-            //   delay: 5000,
-            //   disableOnInteraction: false,
-            // }}
-            breakpoints={{
-              0: {
-                slidesPerView: 1,
-                spaceBetween: 10,
-              },
-              640: {
-                slidesPerView: 2,
-                spaceBetween: 20,
-              },
-              768: {
-                slidesPerView: 2,
-                spaceBetween: 20,
-              },
-              992: {
-                slidesPerView: 3,
-                spaceBetween: 20,
-              },
-              1201: {
-                slidesPerView: 4,
-                spaceBetween: 28,
-              },
-            }}
-            className="new-in-Shop-slider"
-          >
+          <Slider {...settings} className="new-in-Shop-slider">
             {products.map((product, index) => {
               const firstVariant = flattenConnection(product?.variants)[0];
 
@@ -90,11 +116,26 @@ export function NewInTheShop({products, title, locale}) {
               };
 
               return (
-                <SwiperSlide key={index}>
+                <div key={index} className='h-full'>
                   <div className="slide-item p-[15px] bg-white relative">
                     <div className="product-card">
                       <div className="product-card-inner">
-                        <div className={`sale-label ${isDiscounted(price, compareAtPrice) ? "bg-[#9a2ea3]" : '' }  text-white p-[5px] leading-none uppercase text-[13px] absolute right-0 top-0 z-[1]`}> {(isDiscounted(price, compareAtPrice)) &&   (<>SALE <span>{discountedPer(price,compareAtPrice)}</span></> )}  </div>
+                        <div
+                          className={`sale-label ${
+                            isDiscounted(price, compareAtPrice)
+                              ? 'bg-[#9a2ea3]'
+                              : ''
+                          }  text-white p-[5px] leading-none uppercase text-[13px] absolute right-0 top-0 z-[1]`}
+                        >
+                          {isDiscounted(price, compareAtPrice) && (
+                            <>
+                              SALE
+                              <span>
+                                {discountedPer(price, compareAtPrice)}
+                              </span>
+                            </>
+                          )}
+                        </div>
                         <Link
                           to={`/products/${product.handle}`}
                           prefetch="intent"
@@ -114,7 +155,7 @@ export function NewInTheShop({products, title, locale}) {
                           </div>
                         </Link>
                         <h4 className="pro-name text-[13px] text-[#2380b1] font-normal pt-[20px]">
-                          {productTranslate(product,'title',locale)}
+                          {productTranslate(product, 'title', locale)}
                           {/* {console.log(product)} */}
                         </h4>
                         {/* <div className="rating flex gap-[6px] text-[#666666] mt-[12px]">
@@ -125,27 +166,25 @@ export function NewInTheShop({products, title, locale}) {
                         <IconStar className={'w-[17px] h-[15px]'} />
                       </div> */}
                         <div className="price text-[20px] text-black mt-[8px] flex flex-wrap items-center font-['OpenSans'] gap-x-[15px] gap-y-[10px] leading-none">
-                          
-                            {/* CHF 88.10 */}
-                            {/* {isDiscounted(price, compareAtPrice) && 
+                          {/* CHF 88.10 */}
+                          {/* {isDiscounted(price, compareAtPrice) && 
                             console.log(product)
                             } */}
-                            
-                            {isDiscounted(price, compareAtPrice) && (
-                              <CompareAtPrice
-                                // className={'opacity-50'}
-                                data={compareAtPrice}
-                              />
-                            )}
-                              {isDiscounted(price, compareAtPrice) && (
-                                 <DiscountPercentage
-                                 // className={'opacity-50'}
-                                 price={price}
-                                 compareAtPrice={compareAtPrice}
-                               />
-                              )
-                              }
-                         
+
+                          {isDiscounted(price, compareAtPrice) && (
+                            <CompareAtPrice
+                              // className={'opacity-50'}
+                              data={compareAtPrice}
+                            />
+                          )}
+                          {isDiscounted(price, compareAtPrice) && (
+                            <DiscountPercentage
+                              // className={'opacity-50'}
+                              price={price}
+                              compareAtPrice={compareAtPrice}
+                            />
+                          )}
+
                           {/* <span className="pd-price bg-[#b7d4e9] p-[5px] text-white">
                              {discountedPer(price, compareAtPrice)}
                           </span> */}
@@ -162,19 +201,17 @@ export function NewInTheShop({products, title, locale}) {
                             )} */}
                         </div>
                         <div className="buy-now-btn flex flex-col gap-[15px] absolute top-1/2 -translate-y-1/2 left-0 w-full right-0 mx-auto p-[20px] h-full bg-[#dbd4e9f2] items-center justify-center">
-                        
                           <button className='!p-[8.2px_16px] text-[20px] rounded-[5px] bg-transparent border-[2px] border-[#9a2ea3] text-[#9a2ea3] font-["OpenSans"] hover:bg-[#9a2ea3] hover:text-white leading-none transition-all duration-500 w-full'>
-                          <Link
-                          to={`/products/${product.handle}`}
-                          prefetch="intent"
-                         
-                        >
-                            <Text as="span" className="block !text-[20px]">
-                              {translate("view_detail",locale)}
-                            </Text>
+                            <Link
+                              to={`/products/${product.handle}`}
+                              prefetch="intent"
+                            >
+                              <Text as="span" className="block !text-[20px]">
+                                {translate('view_detail', locale)}
+                              </Text>
                             </Link>
                           </button>
-                         
+
                           <AddToCartButton
                             lines={[
                               {
@@ -197,30 +234,10 @@ export function NewInTheShop({products, title, locale}) {
                       </div>
                     </div>
                   </div>
-                </SwiperSlide>
+                </div>
               );
             })}
-          </Swiper>
-          <div
-            id="swiper-button-prev-new-in-Shop"
-            className="absolute left-0 md:left-[-20px] top-1/2 -translate-y-1/2 w-[40px] h-[45px] z-[1] transition-all duration-500 flex items-center justify-center"
-          >
-            <img
-              className="w-full h-full object-contain"
-              src="https://cdn.shopify.com/s/files/1/0787/1352/0419/files/left-a.png?v=1688967073"
-              alt=""
-            />
-          </div>
-          <div
-            id="swiper-button-next-new-in-Shop"
-            className="absolute right-0 md:right-[-20px] top-1/2 -translate-y-1/2 w-[40px] h-[45px] z-[1] transition-all duration-500 flex items-center justify-center"
-          >
-            <img
-              className="w-full h-full object-contain"
-              src="https://cdn.shopify.com/s/files/1/0787/1352/0419/files/right-a.png?v=1688967073"
-              alt=""
-            />
-          </div>
+          </Slider>
         </div>
       </div>
     </section>
